9070722b520146145b89d15ac35e8ca5989c2304
Jesse <jesseguerrero1991@gmail.com>
2023-11-23 22:36:01 -0600

    Created outline of API

--- src/main/java/com/jessenerio/email_service/controller/WebAPI.java
+++ src/main/java/com/jessenerio/email_service/controller/WebAPI.java
@@ -1,14 +1,40 @@
-//package com.jessenerio.email_service.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class WebAPI {
-//    @GetMapping("/api")
-//    public String hello(Model model) {
-////        model.addAttribute("message", "Hello, Spring Web!");
-//        return "index";
-//    }
-//}
+package com.jessenerio.email_service.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WebAPI {
+    @PostMapping("/broadcast")
+    public String broadcast(Model model) {
+        System.out.println("Broadcasting");
+        return "fragments/broadcast";
+    }
+
+    @PostMapping("/manage")
+    public String manage(Model model) {
+        System.out.println("Managing");
+        return "fragments/manage";
+    }
+
+    @PostMapping("/scheduler")
+    public String schedule(Model model) {
+        System.out.println("Scheduling");
+        return "fragments/scheduler";
+    }
+
+    @PostMapping("/test")
+    public String test(Model model) {
+        System.out.println("Testing");
+        return "fragments/test";
+    }
+
+    @PostMapping("/write")
+    public String write(Model model) {
+        System.out.println("Writing");
+        return "fragments/write";
+    }
+}
